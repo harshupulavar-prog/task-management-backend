b1b23814489f3b86a4e25633f758501a32db6d75
Harshi <harshipulavara0514@gmail.com>
2026-09-25 15:56:08 +0530

    allowed all origins

--- main.py
+++ main.py
@@ -27,9 +27,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://task-management-frontend-m1409xjlp-harshupulavar-prog.vercel.app"
+        "*",
+       
     ],
     allow_credentials=True,
     allow_methods=["*"],
