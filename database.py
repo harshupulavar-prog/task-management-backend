7fa2180f3b5599ea76f5ff7bf182c60da8a41754
Harshi <harshipulavara0514@gmail.com>
2026-09-25 16:28:53 +0530

    connected neon postgres db

--- database.py
+++ database.py
@@ -1,13 +1,10 @@
 import os
-
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+load_dotenv()
 
-
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "sqlite:///./tasks.db"
-)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tasks.db")
 
 
 if DATABASE_URL.startswith("sqlite"):
